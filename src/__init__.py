63d04992611ab747ee851092bef4a570d4bdd384
Mamiglia <33762284+Mamiglia@users.noreply.github.com>
2024-06-11 22:26:45 +0200

    Use Weights arithmetics for hessian accumulation

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
 from .fisher import hess
 from .hf_extension import HessianCallback, fisher_matrix
+from .weights_wrapper import Weights
--- src/hf_extension.py
+++ src/hf_extension.py
@@ -6,11 +6,12 @@ from types import MethodType
 from transformers import Pipeline
 from transformers.utils.generic import ModelOutput
 from .fisher import hess
+from .weights_wrapper import Weights
 
 class HessianCallback:
     def __init__(self, model):
         self.model = model
-        self.hessian = [torch.zeros_like(param, requires_grad=False) for param in model.parameters()]
+        self.hessian = Weights.from_model(model, zero_init=True)
         self.num_samples = 0
 
     def __call__(self, outputs: ModelOutput):
@@ -22,11 +23,11 @@ class HessianCallback:
         
     def add_sample(self, sample_hessian: List[torch.Tensor]):
         self.num_samples += 1
-        self.hessian = [H + sample_h for H, sample_h in zip(self.hessian, sample_hessian)]
+        self.hessian += sample_hessian
     
     @torch.no_grad()
-    def get_hessian(self) -> List[torch.Tensor]:
-        return [hessian / self.num_samples for hessian in self.hessian]
+    def get_hessian(self) -> Weights:
+        return self.hessian / self.num_samples
     
     
 def add_callback(func, callback):
@@ -37,7 +38,7 @@ def add_callback(func, callback):
     return new_func
 
 
-def fisher_matrix(pipe: Pipeline, dataset: Any):
+def fisher_matrix(pipe: Pipeline, dataset: Any) -> Weights:
     """Computes the Fisher Information Matrix diagonal approximation for the given dataset and model pipeline.
         Args:
             pipe (Pipeline): the pipeline model
@@ -52,7 +53,7 @@ def fisher_matrix(pipe: Pipeline, dataset: Any):
     pipe.get_inference_context = lambda *_: torch.enable_grad   # enable gradient computation
     pipe._forward = MethodType(add_callback(pipe._forward, hess_callback), pipe) # add the callback to the forward method
 
-    predicted = pipe(dataset)
+    predicted = list(pipe(dataset, batch_size=1))
 
     hessian = hess_callback.get_hessian()
     return hessian
