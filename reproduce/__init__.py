a17645b0d4dfadac702b73b7c943a5471cd249aa
Mamiglia <33762284+Mamiglia@users.noreply.github.com>
2024-06-27 17:29:58 +0200

    fix imports in reproduce folder

--- reproduce/__init__.py
+++
@@ -1,7 +0,0 @@
-from ..src.computation.fisher import hess
-from ..src.computation.hf_extension import HessianCallback, fisher_matrix
-from ..src.merging.base import Merger, weighted_merging, WeightedMerger
-from ..src.merging.stock import StockMerger
-from ..src.merging.ties import TIESMerger, ties_merging
-from ..src.evaluation import evaluate_glue_pipeline
-from ..src.arithmetics.weights_wrapper import ArchitectureTensor
---
+++ reproduce/src.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Add the src directory to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.insert(0, parent_dir)
+
+from src import *
