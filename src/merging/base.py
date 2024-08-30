cdf1a62ea0bf6c5ed8583e0ff937fc028ab4362c
Mamiglia <33762284+Mamiglia@users.noreply.github.com>
2024-08-30 13:48:43 +0200

    model_merge decorator for handling of common operations while merging

--- src/merging/base.py
+++ src/merging/base.py
@@ -1,85 +1,59 @@
+from copy import deepcopy
 import torch
 from torch import nn, Tensor
-from functools import cached_property
-from src.arithmetics.weights_wrapper import ArchitectureTensor
-from typing import Iterable, Optional
+from src.arithmetics.weights_wrapper import StateDict
+from typing import Iterable, Optional, Callable
 from transformers import pipeline 
 
-def weighted_merging(models: Iterable[Tensor], weights: Optional[Iterable[float | Tensor]] = None, eps:float=1e-8)-> Tensor:
-    """
-    Merges the models with the given weights.
-    
-    Args:
-    - models (list): list of models to be merged
-    - weights (list): list of weights to be used for merging, if not given will merge with equal weights
-    
-    Returns:
-    - Tensor: merged model
-    """
-    if weights is None:
-        weights = torch.ones(len(models), dtype=torch.float32)
-    else:
-        weights = torch.tensor(weights, dtype=torch.float32)
-    
-    merged_weights = sum(w * m for w, m in zip(weights, models))
-    merged_weights /= torch.clamp(sum(weights), min=eps)
-    
-    return merged_weights
-
-class Merger:
-    '''Generic Merger abstract class'''
-    def __init__(self, 
-                 models:Iterable[str | nn.Module], 
-                 task:Optional[str] = None, 
-                 passthrough_layers:Iterable[str]=[], 
-                 **merge_args):
-        self.load_models(models, task) # stores architecture, tensors and pipes
-        self.passthrough_layers = passthrough_layers
+def statedict2model(sd:StateDict, model:str|nn.Module|dict, **kwargs) -> str|nn.Module|dict:
+    '''Converts a state_dict to a model'''
+    if isinstance(model, str):
+        task = kwargs.get('task')
+        pipe = pipeline(task, model, framework='pt', device='cpu')
+        missing_keys = pipe.model.load_state_dict(sd, strict=False)
+        print('Missing keys:', missing_keys)
+        return pipe
+    if isinstance(model, nn.Module):
+        model = deepcopy(model)
+        model.load_state_dict(sd, strict=False)
+        return model
+    if isinstance(model, dict):
+        return sd
+    raise ValueError('Model must be either a path to a huggingface model, a nn.Module or a dictionary')
         
-    @cached_property
-    def baseline(self) -> torch.Tensor:
-        '''Returns the baseline model, the mean of the models to be merged. 
-            This is used as a reference point for merging, when some values cannot be merged normally'''
-        return weighted_merging(self.tensors)
-
-    def load_models(self, models:Iterable[str | nn.Module], task:Optional[str] = None):
-        if isinstance(models[0], str):
-            assert task is not None, 'Task must be provided when models are huggingface pipelines'
-            self.pipes = [self.hf_load(model, task) for model in models]
-            models = [pipe.model for pipe in self.pipes]
-        else: 
-            self.pipes = models
-
-        self.arch = ArchitectureTensor(models[0])
-        self.tensors = [self.arch.to_tensor(model) for model in models]
-
-    def tensor2model(self, merged_weights:torch.Tensor) -> nn.Module:
-        # handle non-grad values, not copying them
-        requires_grad = self.arch.grad_mask()
-        is_nan = torch.isnan(merged_weights)
-        avoid_layers = self.arch.name_mask(self.passthrough_layers)
-        passthrough_mask = ~requires_grad | is_nan | avoid_layers
-
-        if passthrough_mask.any():
-            print(f'Passing through {passthrough_mask.sum()} weights, of which: {is_nan.sum()} are NaN and {(~requires_grad).sum()} don\'t require gradient.')
-            merged_weights[passthrough_mask] = self.baseline[passthrough_mask]
-
-        return self.arch.to_model(merged_weights)
     
-    @staticmethod
-    def hf_load(model:str, task:str):
-        '''Load a huggingface pipeline'''
-        return pipeline(task, model=model, device='cpu', framework='pt')
-    
-    def merge(self):
-        raise NotImplementedError('This method should be implemented in the child class')
-
-
-class WeightedMerger(Merger):
-    def __init__(self, models:Iterable[Tensor], weights: Optional[Iterable[float | Tensor]] = None, **merge_args):
-        super().__init__(models, **merge_args)
-        self.weights = weights
-
-    def merge(self) -> nn.Module:
-        merged_weights = weighted_merging(self.tensors, self.weights)
-        return self.tensor2model(merged_weights)
+def model_merge(func: Callable[[Iterable[StateDict]], StateDict]) -> Callable:
+    def wrapper(models: Iterable[str | nn.Module | dict],
+                passthrough_layers:Iterable[str] = [],
+                *args, **kwargs) -> nn.Module:
+        if isinstance(models[-1], str):
+            task = kwargs.get('task')
+            state_dicts = [StateDict.from_hf(model, device='cpu', **kwargs) for model in models]
+        elif isinstance(models[-1], nn.Module):
+            state_dicts = [StateDict.from_model(model) for model in models]
+        elif isinstance(models[-1], dict):
+            state_dicts = [StateDict(model) for model in models]
+        else:
+            raise ValueError('Models must be either paths to huggingface models, nn.Modules or state_dicts')
+        
+        state_dicts = [sd.filter_values(lambda x: x.requires_grad) for sd in state_dicts]
+        
+        # The passthrough layers are not included in the standard merging process
+        # rather for those layers, the finetuned models are averaged and integrated 
+        # into the final merged weights
+        passthrough_weights = {}
+        if len(passthrough_layers) > 0:
+            pass_sd = state_dicts
+            if 'base_index' in kwargs:
+                base_index = kwargs['base_index']
+                pass_sd = state_dicts[:base_index] + state_dicts[base_index+1:]
+            pass_sd = [sd.subset(passthrough_layers) for sd in pass_sd]
+            passthrough_weights = sum(pass_sd, start=StateDict.zeros_like(pass_sd[0])) / len(pass_sd)
+            
+            state_dicts = [sd.remove(passthrough_layers) for sd in state_dicts]
+        
+        merged_weights = func(state_dicts, *args, **kwargs)
+        merged_weights.update(passthrough_weights)
+        
+        return statedict2model(merged_weights, models[-1], **kwargs)
+    return wrapper
